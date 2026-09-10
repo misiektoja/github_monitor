38d9202810ac50eeff4628166330c041ab7bedc2
misiektoja <misiektoja-github@rm-rf.ninja>
2026-09-10 11:21:22 +0200

    fix(diagnostics): print the liveness banner once a day by default

--- github_monitor.py
+++ github_monitor.py
@@ -295,7 +295,7 @@ TRACK_CONTRIB_CHANGES = False
 
 # How often to print a "liveness check" message to the output; in seconds
 # Set to 0 to disable
-LIVENESS_CHECK_INTERVAL = 43200  # 12 hours
+LIVENESS_CHECK_INTERVAL = 86400  # 24 hours
 
 # URL used to verify internet connectivity at startup
 CHECK_INTERNET_URL = GITHUB_API_URL
