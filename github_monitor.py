743d5a86d57083dc72ec5cc82269b34d41864fb2
misiektoja <misiektoja-github@rm-rf.ninja>
2026-09-18 11:03:27 +0200

    build: declare version 2.7.1

--- github_monitor.py
+++ github_monitor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """
 Author: Michal Szymanski <misiektoja-github@rm-rf.ninja>
-v2.7
+v2.7.1
 
 OSINT tool implementing real-time tracking of GitHub users activities including profile and repositories changes:
 https://github.com/misiektoja/github_monitor/
@@ -18,7 +18,7 @@ colorama (optional, improves classic Windows Command Prompt colour support)
 wcwidth (optional, measures wide characters correctly when TRUNCATE_CHARS is set)
 """
 
-VERSION = "2.7"
+VERSION = "2.7.1"
 
 # ---------------------------
 # CONFIGURATION SECTION START
